814fce6ec9121d7d88d29de3f779f2605ead8b31
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2024-10-08 00:31:23 -0500

    Add version string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! Driver-code
 
+use std::env::consts::{ARCH, OS};
 use std::io::{Error, ErrorKind};
 use std::{env, io};
 
@@ -21,8 +22,29 @@ fn main() -> Result<(), Error> {
         "d" => uast::process_uast,
         "i" => iast::devanāgarī_to_iast,
         "g" => gu::devanāgarī_to_gujarātī,
+        "-v" => {
+            #[cfg(debug_assertions)]
+            const BUILD_TYPE: &'static str = "debug";
+            #[cfg(not(debug_assertions))]
+            const BUILD_TYPE: &'static str = "release";
+
+            println!(
+                "{} {} ({BUILD_TYPE} build, {OS} [{ARCH}])",
+                env!("CARGO_PKG_NAME"),
+                env!("CARGO_PKG_VERSION")
+            );
+            println!("For web version, visit `https://uast.dev`");
+            println!("For citations, visit `https://arxiv.org/abs/2203.14277`");
+
+            return Ok(());
+        }
+        "-h" => {
+            println!("Usage: uast [d|i|g]");
+
+            return Ok(());
+        }
         _ => {
-            return Err(Error::new(ErrorKind::InvalidInput, "Usage: uast [d|i|h|g]"));
+            return Err(Error::new(ErrorKind::InvalidInput, "Usage: uast [d|i|g]"));
         }
     };
 
