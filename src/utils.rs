db998f9e2c98f483c977189c620903a410e8f39e
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2026-01-09 23:10:31 +0530

    Perf optimizations in /src/utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,17 @@ pub(crate) fn split_line_and_convert<F>(f: F, s: &str) -> String
 where
     F: Fn(&str) -> String,
 {
-    s.split_whitespace().map(f).collect::<Vec<_>>().join(" ")
+    let mut res = String::new();
+    let mut first = true;
+
+    for word in s.split_whitespace() {
+        if !first {
+            res.push(' ');
+        }
+        res.push_str(&f(word));
+        first = false;
+    }
+    res
 }
 
 pub(crate) fn binary_search<U, V, F>(arr: &[(char, U)], c: char, f: F) -> Option<V>
@@ -12,21 +22,32 @@ where
     U: Copy,
     F: Fn(U) -> V,
 {
-    let mut i = 0_isize;
-    let mut j = (arr.len() - 1) as isize;
+    if arr.is_empty() {
+        return None;
+    }
+
+    let mut i = 0;
+    let mut j = arr.len() - 1;
+    let p = arr.as_ptr();
 
     while i <= j {
-        let m = (i + j) / 2;
-        let v = arr[m as usize];
+        let m = i + (j - i) / 2;
+        let v = unsafe { *p.add(m) };
 
         if c == v.0 {
             return Some(f(v.1));
         }
 
         if c > v.0 {
-            i = m + 1;
+            i = match m.checked_add(1) {
+                Some(val) => val,
+                None => return None,
+            };
         } else {
-            j = m - 1;
+            j = match m.checked_sub(1) {
+                Some(val) => val,
+                None => return None,
+            };
         }
     }
 
